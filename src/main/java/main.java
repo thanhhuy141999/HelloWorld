8d23669388b9e92192b1841f707b768d0307f2ff
Huy Nguyen Bao Thanh <thanhhuy1419991@gmail.com>
2022-01-07 17:42:01 +0700

    4th commit

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,7 +1,7 @@
 public class main {
     public static void main(String args []){
-        System.out.println("hello");
-        System.out.println("hello3");
+        System.out.println("hello1");
+        System.out.println("hello2");
         System.out.println("hello3");
     }
 }
